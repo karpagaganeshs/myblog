473477181ac07c81c954a4e2535abd6ce226cc0f
karpagaganeshs <karpagaganeshsubramanian@gmail.com>
2025-08-22 18:14:11 +0530

    fix: remove @tailwindcss/vite from config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [tailwindcss(),react()],
+  plugins: [react()],
 })
